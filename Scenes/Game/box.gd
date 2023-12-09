8b3ce01027024977914aea9bca9a42f008e7cfb4
Emily Krohn <emilykrohn2@gmail.com>
2023-12-09 07:44:23 -0800

    Player can delete boxes with space bar
    When player is in range of a box and they press space, they can queue free that box from the scene

--- Scenes/Game/box.gd
+++ Scenes/Game/box.gd
@@ -1,12 +1,7 @@
 extends StaticBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_key_pressed(KEY_SPACE) and $%EnteredRange.in_range:
+		queue_free()
 
