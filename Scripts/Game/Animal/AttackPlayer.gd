dfddc2fb19ffbac668a5a4188ddcc376f135a3a7
Emily Krohn <emilykrohn2@gmail.com>
2023-12-10 08:22:53 -0800

    Boar chases player

--- Scripts/Game/Animal/AttackPlayer.gd
+++ Scripts/Game/Animal/AttackPlayer.gd
@@ -1,3 +1,7 @@
 extends Node
 
+const SPEED := 50
 
+func attack(delta):
+	var player_position = $"../../Player".global_position
+	$"../".position += (player_position - $"../".position).normalized() * SPEED * delta
--- Scripts/Game/Animal/boar.gd
+++ Scripts/Game/Animal/boar.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
+var is_attacking := false
+
 func _process(delta):
-	$AnimalFunctions/AnimalMovement.movement(delta)
+	if not is_attacking:
+		$AnimalFunctions/AnimalMovement.movement(delta)
+	else:
+		$AttackPlayer.attack(delta)
 	$AnimalFunctions/PlayAnimalAnimation.play_animation()
 
 func _on_area_2d_body_entered(body):
@@ -11,3 +16,7 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body.name == "Player":
 		$AnimalFunctions/AnimalTakeDamage.can_be_damaged = false
+
+
+func _on_spawn_cooldown_timeout():
+	is_attacking = true
