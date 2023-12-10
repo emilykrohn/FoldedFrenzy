9dac700bbcbb7837a68856d866cefa53aefd4c01
Emily Krohn <emilykrohn2@gmail.com>
2023-12-09 21:02:29 -0800

    Add boar to scenes that can spawn

--- Scripts/Game/Box/box.gd
+++ Scripts/Game/Box/box.gd
@@ -1,9 +1,15 @@
 extends StaticBody2D
 
 @export var stag_scene: PackedScene
+@export var boar_scene: PackedScene
+
+var scenes = []
+func _ready():
+	scenes.append(stag_scene)
+	scenes.append(boar_scene)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_key_pressed(KEY_SPACE) and $%EnteredRange.in_range:
 		queue_free()
-		$%Spawn.spawn_stag(stag_scene)
+		$%Spawn.spawn_stag(scenes[randi() % scenes.size()])
